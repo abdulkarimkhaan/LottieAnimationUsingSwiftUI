b0db33b8c11d8437c4d9c1d53eddf7bc9bfeae63
Abdul Karim Khan <abdulkarimkhan.cs@hotmail.com>
2023-08-15 07:11:41 +0500

    Initial Commit

---
+++ MyAnimatedApp/MyAnimatedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAnimatedAppApp.swift
+//  MyAnimatedApp
+//
+//  Created by Abdul Karim Khan on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyAnimatedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
